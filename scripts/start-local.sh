835a23b671f539a0ff14a61f96f323b584a3b6a2
Fernando Martínez Altolaguirre <fmartinez.altolaguirre@gmail.com>
2026-07-21 11:20:29 +0200

    Add local Docker platform and Jenkins stack

--- scripts/start-local.sh
+++ scripts/start-local.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+set -e
+
+echo "Starting Azure LAMP Platform..."
+
 docker compose up -d
 
+echo ""
+echo "Running containers:"
 docker ps
+
+echo ""
+echo "Jenkins:"
+echo "http://localhost:8080"
+
+echo ""
+echo "PHP Application:"
+echo "http://localhost"
+
+echo ""
+echo "phpMyAdmin:"
+echo "http://localhost:8081"
